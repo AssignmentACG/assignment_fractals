a27844667790e6e665abd5cbd26d43fb19b77d84
Epix <git@exz.me>
2016-06-18 17:44:37 +0800

    add more fractal; move css

--- fractals_app.py
+++ fractals_app.py
@@ -4,6 +4,7 @@ import os
 
 from PIL import Image, ImageDraw
 from flask import Flask, send_file, send_from_directory
+from werkzeug.routing import FloatConverter
 
 app = Flask(__name__)
 CACHE_PATH = 'tile'
@@ -11,6 +12,13 @@ TILE_SIZE = 256
 MAX_ITER = 100
 ESCAPE_RADIUS = 4
 BLACK_COLOR = (0, 0, 0)
+
+
+class NegativeFloatConverter(FloatConverter):
+    regex = r'-?\d+(\.\d+)?'
+
+
+app.url_map.converters['float'] = NegativeFloatConverter
 
 
 def gradient2color_map(gradient):
@@ -38,6 +46,7 @@ GRADIENT = {0: (41, 137, 204),
             64: (217, 159, 0),
             100: (255, 255, 255),
             }
+# GRADIENT = {0: (255, 0, 0), 100: (0, 255, 255)}
 COLOR_MAP = gradient2color_map(GRADIENT)
 
 
@@ -46,29 +55,96 @@ def homepage():
     return send_from_directory('static', 'index.html')
 
 
-@app.route('/tile/<string:f_type>_<string:f_formula>_<int:z>_<int:x>_<int:y>.png', methods=['GET'])
+@app.route('/tile/<string:f_type>_<string:f_formula>_<int:z>_<float:x>_<float:y>.png', methods=['GET'])
 def get_fractals(f_type, f_formula, z, x, y):
     path = os.path.join(CACHE_PATH,
-                        '{f_type}_{f_formula}_{z}_{x}_{y}.png'.format(f_type=f_type, f_formula=f_formula, z=z, x=x,
-                                                                      y=y))
+                        '{f_type}_{f_formula}_{z}_{x}_{y}.png'
+                        .format(f_type=f_type, f_formula=f_formula, z=z, x=x, y=y))
+
     if not os.path.isfile(path):
         # if file not exist, generate
-        if f_type == 'mandelbrot':
-            make_mandelbrot(f_formula, z, x, y, path)
+        if f_type == 'multibrot':
+            make_multibrot(f_formula, z, x, y, path)
+        elif f_type == 'julia':
+            make_julia(f_formula, z, x, y, path)
+        elif f_type == 'burning':
+            make_burning(f_formula, z, x, y, path)
     return send_file(path)
 
 
-def make_mandelbrot(f_formula, zoom, x, y, path):
+def make_multibrot(f_formula, zoom, x, y, path):
+    param1 = float(f_formula.split(',')[0])
+    if not param1:
+        param1 = 2
+    im = Image.new('RGB', (TILE_SIZE, TILE_SIZE))
+    draw = ImageDraw.Draw(im)
+    max_distance = 0
+    min_distance = 10e5
+    for p_x in range(TILE_SIZE):
+        for p_y in range(TILE_SIZE):
+            a_x = (p_x / TILE_SIZE + x) / (2 ** zoom)
+            a_y = (p_y / TILE_SIZE + y) / (2 ** zoom)
+            c = a_x + a_y * 1j
+            z = c
+            init_z = z
+            distance = 10e5
+            for i in range(MAX_ITER):
+                z = z ** param1 + c
+                distance = min(distance, abs(init_z - z))
+                if abs(z) > ESCAPE_RADIUS:
+                    draw.point((p_x, p_y), fill=COLOR_MAP[min(int(1 / (distance + 1e-5)), 100)])
+                    break
+            else:
+                draw.point((p_x, p_y), fill=COLOR_MAP[min(int(1 / (distance + 1e-5)), 100)])
+            max_distance = max(max_distance, distance)
+            min_distance = min(min_distance, distance)
+    # print(max_distance, min_distance)
+    im.save(path)
+
+
+def make_julia(f_formula, zoom, x, y, path):
+    param1, param2, param3 = map(float, (f_formula.split(',')))
+    if not param1:
+        param1 = 2
+    im = Image.new('RGB', (TILE_SIZE, TILE_SIZE))
+    draw = ImageDraw.Draw(im)
+    max_distance = 0
+    min_distance = 10e5
+    for p_x in range(TILE_SIZE):
+        for p_y in range(TILE_SIZE):
+            a_x = (p_x / TILE_SIZE + x) / (2 ** zoom)
+            a_y = (p_y / TILE_SIZE + y) / (2 ** zoom)
+            z = a_x + a_y * 1j
+            c = param2 + param3 * 1j
+            init_z = z
+            distance = 10e5
+            for i in range(MAX_ITER):
+                z = z ** param1 + c
+                distance = min(distance, abs(init_z - z))
+                if abs(z) > ESCAPE_RADIUS:
+                    # draw.point((p_x, p_y), fill=COLOR_MAP[min(int(1 / (distance + 1e-5)), 100)])
+                    draw.point((p_x, p_y), fill=COLOR_MAP[i])
+                    break
+            else:
+                draw.point((p_x, p_y), fill=BLACK_COLOR)
+        max_distance = max(max_distance, distance)
+        min_distance = min(min_distance, distance)
+    # print(max_distance, min_distance)
+    im.save(path)
+
+
+def make_burning(f_formula, zoom, x, y, path):
     im = Image.new('RGB', (TILE_SIZE, TILE_SIZE))
     draw = ImageDraw.Draw(im)
     for p_x in range(TILE_SIZE):
         for p_y in range(TILE_SIZE):
             a_x = (p_x / TILE_SIZE + x) / (2 ** zoom)
             a_y = (p_y / TILE_SIZE + y) / (2 ** zoom)
-            z = 0
             c = a_x + a_y * 1j
+            z = 0
             for i in range(MAX_ITER):
-                z = z ** 2 + c
+                z = (abs(z.real) + abs(z.imag) * 1j) ** 2 + c
+
                 if abs(z) > ESCAPE_RADIUS:
                     draw.point((p_x, p_y), fill=COLOR_MAP[i])
                     break
--- static/fractals.coffee
+++ static/fractals.coffee
@@ -1,11 +1,56 @@
-map = L.map 'map', {minZoom: 0}
-mandelbrot_layer = L.tileLayer '/tile/{f_type}_{f_formula}_{z}_{x}_{y}.png',
-  f_type: 'mandelbrot'
-  f_formula: 2
+ParamControl = L.Control.extend({
+  options:
+    position: 'topright'
+  onAdd: (map) ->
+    this_map = map
+    container = L.DomUtil.create('div', 'param-control leaflet-control-layers leaflet-control-layers-expanded');
+    p1 = L.DomUtil.create('input', '', container)
+    p1.setAttribute('id', 'param1')
+    p1.setAttribute('type', 'number')
+    p2 = L.DomUtil.create('input', '', container)
+    p2.setAttribute('id', 'param2')
+    p2.setAttribute('type', 'number')
+    p3 = L.DomUtil.create('input', '', container)
+    p3.setAttribute('id', 'param3')
+    p3.setAttribute('type', 'number')
+    btn = L.DomUtil.create('button', '', container)
+    btn.innerHTML = 'Update'
+    L.DomEvent.addListener btn, 'click', ->
+      this_map.eachLayer((layer)->
+        layer.options.f_formula = [document.getElementById('param1').value,
+          document.getElementById('param2').value,
+          document.getElementById('param3').value].join(',')
+        layer.redraw()
+      )
+      return
+    return container;
+})
+
+
+map = L.map 'map', {minZoom: 0, crs: L.CRS.Simple}
+multibrot_layer = L.tileLayer '/tile/{f_type}_{f_formula}_{z}_{x}_{y}.png',
+  f_type: 'multibrot'
+  f_formula: '2,0,0'
+  continuousWorld: true
   noWrap: true
-  maxBounds: [[0, 0], [2, 2]]
+  maxBounds: [[-2, -2], [2, 2]]
 .addTo(map)
+julia_layer = L.tileLayer '/tile/{f_type}_{f_formula}_{z}_{x}_{y}.png',
+  f_type: 'julia'
+  f_formula: '2,-0.4,0.6'
+  continuousWorld: true
+  noWrap: true
+  maxBounds: [[-2, -2], [2, 2]]
+burning_layer = L.tileLayer '/tile/{f_type}_{f_formula}_{z}_{x}_{y}.png',
+  f_type: 'burning'
+  f_formula: '2,-0.4,0.6'
+  continuousWorld: true
+  noWrap: true
+  maxBounds: [[-2, -2], [2, 2]]
 layers =
-  'mandelbrot': mandelbrot_layer
+  'multibrot': multibrot_layer
+  'julia': julia_layer
+  'burning ship': burning_layer
 lc = L.control.layers(layers, {}, {position: 'topleft', collapsed: false}).addTo(map)
-map.setView([0, 0], 1)
+map.addControl(new ParamControl())
+map.setView([0, 0], 0)
